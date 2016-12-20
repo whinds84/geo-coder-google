fc4b71f49fb5620d81073acb51724a1787e6e3e1
jipipayo <daniel.remeseiro@gmail.com>
2016-12-20 21:18:16 +0100

    removed param sensor, and some reformating
    param sensor is no longer required, and generates deprecated warning messages.
    https://developers.google.com/maps/documentation/geocoding/intro?hl=en

--- lib/Geo/Coder/Google/V3.pm
+++ lib/Geo/Coder/Google/V3.pm
@@ -22,17 +22,16 @@ sub new {
     my $language = delete $param{language} || delete $param{hl};
     my $region   = delete $param{region}   || delete $param{gl};
     my $oe       = delete $param{oe}       || 'utf8';
-    my $sensor   = delete $param{sensor}   || 0;
-    my $channel  = delete $param{channel}   || undef;
+    my $channel  = delete $param{channel}  || undef;
     my $client   = delete $param{client}   || '';
     my $key      = delete $param{key}      || '';
     my $components = delete $param{components};
    
     bless { 
         ua => $ua, host => $host, language => $language, 
-        region => $region, oe => $oe, sensor => $sensor, channel => $channel,
-        client => $client, key => $key,
-        components => $components,
+        region => $region, oe => $oe, sensor => $sensor,
+        channel => $channel, client => $client,
+        key => $key, components => $components,
     }, $class;
 }
 
@@ -84,9 +83,8 @@ sub geocode {
     $query_parameters{language} = $self->{language} if defined $self->{language};
     $query_parameters{region} = $self->{region} if defined $self->{region};
     $query_parameters{oe} = $self->{oe};
-    $query_parameters{sensor} = $self->{sensor} ? 'true' : 'false';
-    my $components_params = $self->_get_components_query_params;
     $query_parameters{channel} = $self->{channel} if defined $self->{channel};
+    my $components_params = $self->_get_components_query_params;
     $query_parameters{components} = $components_params if defined $components_params;
     $query_parameters{key} = $self->{key} if defined $self->{key};
     $uri->query_form(%query_parameters);
